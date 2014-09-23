5f9e437ff06fa66db97336f75906f7eb338213fc
Sam Sheffres <ssheffres@gmail.com>
2014-09-23 21:34:01 +0700

    Cleaned up unused Instance Vars

--- stretchy.cr
+++ stretchy.cr
@@ -4,16 +4,13 @@ class Stretchy
   @http_client = HTTP::Client
   
   def initialize(host = "127.0.0.1", port = "9200")
-    @host = host
-    @port = port
-    @es_url = "http://#{@host}:#{@port}"
+    @es_url = "http://#{host}:#{port}"
   end
 
   def search(query)
     response = post_json("#{@es_url}/_search", query)
     puts response
   end
-
 
   def post_json(url, json)
     Json.parse(@http_client.post(url, json).body.not_nil!)
